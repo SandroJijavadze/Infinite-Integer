1b7b59b15ae8ac7ce06bd6cd2f93c78dd5af6759
SandroJijavadze <sjijavadze@sdsu.edu>
2017-10-15 16:03:44 +0400

    Implemented and tested Add(), Implemented but not tested Subtract()

--- InfInt/InfInt.cs
+++ InfInt/InfInt.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace InfInt
 {
-    class InfInt: IComparable<InfInt>
+    class InfInt : IComparable<InfInt>
     {
         private List<int> values = new List<int>();
         private bool sign = true; // Positive, if not set negative(false).
         private bool isnull = false;
-
         public InfInt(string infinite)
         {
             int i = 0; // 
-            //int length = 0;
-            if (infinite[0] == '-') {
+            if (infinite[0] == '-')
+            {
                 sign = false;
                 i++;
-                //length--;
             }
             while (infinite[i] == '0' && infinite[i + 1] != '\0')  // Skip zeros in the beginning, if there are any.
                 i++;
 
-            //length += infinite.Length - i; // Length of infint, not counting sign and any zeros in the beginning. 
-
-            while (i < infinite.Length) 
+            while (i < infinite.Length)
             {
                 values.Add((int)Char.GetNumericValue(infinite[i++])); //Int32.Parse(infinite[i++]+"")); 
             }
@@ -37,75 +35,190 @@ namespace InfInt
 
         public int CompareTo(InfInt otherInfInt)
         {
-            if (otherInfInt == null) return 1; 
+            if (otherInfInt == null) return 1;
 
             int i = 0;
             int length = this.values.Count;
-            if (this.values.Count == otherInfInt.values.Count) // If their lengths are equal.
-            {  
-                while (this.values[i] == otherInfInt.values[i] && i < length-1)  // Skip integers that are equal, until last integer.
+
+            if (this.sign != otherInfInt.sign)
+            {
+                if (this.sign == true) // If one is positive and another negative, return accordingly.
+                    return 1;
+                else
+                    return -1;
+            }
+            else if (this.values.Count == otherInfInt.values.Count)
+            {// If their lengths are equal.
+                while (this.values[i] == otherInfInt.values[i] && i < length - 1)  // Skip integers that are equal, until last integer.
                     i++;
-                return this.values[i].CompareTo(otherInfInt.values[i]); // Return comparison result of leftmost integers that are not equal.
+                if (this.sign == true)
+                    return this.values[i].CompareTo(otherInfInt.values[i]); // Return comparison result of leftmost integers that are not equal.
+                else
+                    return -((this.values[i]).CompareTo(otherInfInt.values[i]));
             }
             else
-            {
-                return this.values.Count.CompareTo(otherInfInt.values.Count);
+            { // If their count are not equal, return the higher count if it is positive, return -(minus) that if negative.
+                if (this.sign == true) // if they are positive
+                    return this.values.Count.CompareTo(otherInfInt.values.Count); // return CompareTo of their count.
+                else
+                    return -(this.values.Count.CompareTo(otherInfInt.values.Count));
             }
-            
         }
-        
+
         public override string ToString()
         {
-            if (sign) {
+            if (sign)
                 return string.Join("", values);
+            else
+                return "-" + string.Join("", values);
+        }
+
+        private string MemberwiseAdd(InfInt other, bool otherFirstOperand = false)
+        {
+            string result = "";
+            InfInt firstOperand = null;
+            InfInt secondOperand = null;
+            if (otherFirstOperand)
+            {
+                firstOperand = other;
+                secondOperand = this;
             }
             else
             {
-                return "-" + string.Join("", values);
+                firstOperand = this;
+                secondOperand = other;
             }
-        }
-        
-        public string sub(InfInt otherInfInt) // Subtract.
-        {
-            List<char> newList = new List<char>;
-            int remainder = 0;
-            int index1 = this.values.Count -1; // 
-            int index2 = otherInfInt.values.Count -1;
-            if(this.sign == otherInfInt.sign)
-            {
-                while(index1 != 0 || index2 != 0)
-                {
-                    if((remainder = this.values[index1] - otherInfInt.values[index2]) < 0) 
-                    {
-                        // axal listshi + it sheushvi remainderi char ad gadaqceuli
-                        // ukan yvela noli 9 ianit shecvale pirvel ricxvshi.
-                    }
-                    else if(remainder == 0)
-                    {
-                        // sheagde 0
-                    }
-                    else
-                    {
-                        //
-                        // pirdapir axal listshi sheagde (10 - remainder)
-                    }
 
+            int thisOne = firstOperand.values.Count() - 1;
+            int otherOne = secondOperand.values.Count() - 1;
+            int carry = 0;
+            int addRes = 0;
+            while (thisOne != -1)
+            {  // While all integers are not added.
+                if (otherOne >= 0)  // If not last integer.
+                    addRes = firstOperand.values[thisOne] + secondOperand.values[otherOne] + carry;
+                else // If last integer.
+                    addRes = firstOperand.values[thisOne] + carry;
+                carry = 0;
+                if (addRes >= 10) // IF we have carry, increment it, and decrement the actual integer by 10.
+                {
+                    addRes -= 10;
+                    carry += 1;
+                }
+                result = addRes.ToString() + result;
+                thisOne--;
+                otherOne--;
+            }
+            if (thisOne == -1 && carry > 0)
+                result = carry.ToString() + result;
+            if (otherFirstOperand == true)
+                result = "-" + result;
+            return result;
+        }
+
+        private string MemberwiseSubtract(InfInt other, bool otherFirstOperand = false)
+        {
+            string result = "";
+            InfInt firstOperand = null;
+            InfInt secondOperand = null;
+            if (otherFirstOperand)
+            {
+                firstOperand = other;
+                secondOperand = this;
+            }
+            else
+            {
+                firstOperand = this;
+                secondOperand = other;
+            }
+            int thisOne = firstOperand.values.Count() - 1;
+            int otherOne = secondOperand.values.Count() - 1;
+            int remainder = 0;
+            int subRes = 0;
+            while(thisOne >= 0) // While Last Integer Not Reached 
+            {
+                if (otherOne >= 0) // If second operand has still got , result = first - second - remainder
+                    subRes = firstOperand.values[thisOne] - secondOperand.values[otherOne] - remainder;
+                else               // If no members are left, result = first - remainder
+                    subRes = firstOperand.values[thisOne] - remainder;
+                remainder = 0; //Alreadey used it.
+                if (subRes < 0)  // If result is less than 0
+                {
+                    subRes += 10; // Increment it by 10
+                    remainder++; // Increase remainder
                 }
 
-                // IF overflow, add 1 to in the beginning of list.
-            }// Add else, if their signs are not equal.
+                result = subRes.ToString() + result; // Add result
+                thisOne--;                          // Decrement indices.
+                otherOne--;                 
+            }
+            if (otherFirstOperand == true)  //Assumes that first operand is larger in modulus.
+                result = "-" + result;
+            return result;
         }
 
+        private int ModulusCompareTo(InfInt other)// True if this one is larger in modulus, false otherwise.
+        {
+            bool thisOne = this.sign;
+            bool otherOne = other.sign;
+            int result;
+            // Save their signs.
 
+            //Make them positive.
+            this.sign = true;
+            other.sign = true;
+            result = this.CompareTo(other);
 
+            //Restore signs
+            this.sign = thisOne;
+            other.sign = otherOne;
 
+            return result; // Return result of compareto.
+        }
+
+        public string Add(InfInt other)
+        {
+            string result = "";
+            if (this.CompareTo(other) >= 0)// If first one is larger.
+            { 
+                if (this.sign == true && other.sign == true) // both are positive and first one is larger
+                {        
+                    result = this.MemberwiseAdd(other);
+                }
+                else if (this.sign == false && other.sign == false)// Both are negative, result is negative. other - this, memberwise.
+                {
+                    result = this.MemberwiseAdd(other, true);
+                }
+                else // first one is  positive, second one is negative. larger in modulus - smaller in modulus(memberwise), sign of larger one in modulus.
+                {
+                    if (this.ModulusCompareTo(other) == 0)//If modulus are equal
+                        result = "0";
+                    else if (this.ModulusCompareTo(other) > 0) // If this one has bigger modulus.
+                        result = this.MemberwiseSubtract(other);
+                    else   // The modulus of negative one is larger
+                        result = this.MemberwiseSubtract(other, true);
+                }
+            }
+            else if (this.CompareTo(other) < 0) // Second one is larger
+            {
+                if (other.sign == false)                     // both are negative, result = other + this. negative sign
+                    result = other.MemberwiseAdd(this, true);
+                else if (this.sign == false && other.sign == true)//2.this negative and other positive. result =  larger in modulus - smaller in modulus, sign of larger in modulus.
+                    result = other.MemberwiseSubtract(this, (this.ModulusCompareTo(other) >= 0));
+                else                    // Both are positive. result other + this, positive sgn.
+                    result = other.MemberwiseAdd(this);
+            }
+            return result;
+
+        }
+
+       
         // public void multiply(InfInt object)
-
-        // public void add(InfInt object){
-
     }
 
-
-
 }
+
+
+
+
 
--- InfInt/Program.cs
+++ InfInt/Program.cs
@@ -10,21 +10,57 @@ namespace InfInt
     {
         static void Main(string[] args)
         {
+            
             InfInt someInfInt = new InfInt("65198198191979711");
             List<InfInt> infIntList = new List<InfInt> {
-                new InfInt("65198198191979711"),
-                new InfInt("65198198191979711"),
-                new InfInt("6519819819197971"),
-                new InfInt("06519819819197971"),
-                new InfInt("06519819819197972")
+                /*new InfInt("9999999999"),
+                new InfInt("-1111111111"),
+                */
+                
+                
+                // Cases when first one  is more positive.   4 in total
+                new InfInt("651981982319197971"), // Both Positive, # 1
+                new InfInt("999999999999123"),
 
+                new InfInt("-2319197971"), // Both negative, #2
+                new InfInt("-999999999999123"),
+
+                new InfInt("651123981982319197971"), // Only second one negative. First has larger modulus #3
+                new InfInt("-99923999999999123"),
+
+                new InfInt("651981982311239197971"), // Only second one negative. Second one has larger modulus
+                new InfInt("-9999999123123099999123"),
+
+
+                //Case when they are equal
+                 new InfInt("651981982311239197971"),// #4
+                 new InfInt("651981982311239197971"),
+
+                // Cases when second one is more positive
+                new InfInt("-651981982319197971"),  // Both are negative #5
+                new InfInt("-99123999999123"),
+
+                new InfInt("-999999999999123"), // Only first is negative, second large in modulus # 6
+                new InfInt("9999999999999232123"),
+
+                new InfInt("-999912391499999999123"), // Only first is negative, first large in modulus #7
+                new InfInt("999999999999123"),
+
+                new InfInt("999912391499999999123"), //Both positive. No matter which is larger in modulus. #8
+                new InfInt("999999999999123")
+
+                
             };
-            for(int i = 0; i < 4; i++)
+            for (int i = 0; i <17  ; i += 2) 
             {
-                Console.WriteLine(infIntList[i].CompareTo(infIntList[i+1]));
+                Console.Write($"#{i/2}:  ");
+                Console.WriteLine(infIntList[i].Add(infIntList[i + 1]));
+                
+
             }
+            
+            Console.WriteLine($"{1234.ToString()}");
             Console.ReadLine();
-            // output : 0, 1,0,-1
 
         }
     }
